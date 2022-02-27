b750a9e2f657dbb0d6b279e0a03a09b0e9f3b484
Plamen Atanasov <pl.atanasov89@gmail.com>
2022-02-27 12:23:14 +0200

    remove 3 prev classes

--- cars/carmodel/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import CarModel
-
-admin.site.register(CarModel)
--- cars/usercar/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import UserCar
-
-admin.site.register(UserCar)
