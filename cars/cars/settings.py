f94408cc329d269ded979f2b228c5fcbeeb6a550
Plamen Atanasov <pl.atanasov89@gmail.com>
2022-02-27 13:13:19 +0200

    restored project

--- cars/cars/settings.py
+++ cars/cars/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'carbrand',
-    'carmodel',
-    'usercar',
+    'API',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
